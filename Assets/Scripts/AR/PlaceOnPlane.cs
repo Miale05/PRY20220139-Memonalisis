c0defc9a5d887729e7db147cd254402c19b70810
Juan <josemariacosta_8@hotmail.com>
2022-06-27 03:04:32 -0500

    Inicializacion de proyecto en github

---
+++ Assets/Scripts/AR/PlaceOnPlane.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+[RequireComponent(typeof(ARRaycastManager))]
+public class PlaceOnPlane : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("Insantiates this prefab on a plane at the touch location")]
+    GameObject prefabObject;
+
+    UnityEvent placementUpdate;
+
+    [SerializeField]
+    GameObject visualObject;
+
+    ARRaycastManager raycastManager;
+    static List<ARRaycastHit> hits = new List<ARRaycastHit>();
+
+    public GameObject spawnedObject { get; private set; }
+
+
+    public GameObject placedPrefab {
+        get { return prefabObject; }
+        set { prefabObject = value; }
+    }
+
+    private void Awake() {
+        raycastManager = GetComponent<ARRaycastManager>();
+
+        if (placementUpdate == null) {
+            placementUpdate = new UnityEvent();
+
+            placementUpdate.AddListener(DisableVisual);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!TryGetTouchPosition(out Vector2 touchPosition)) {
+            return;
+        }
+
+        if (raycastManager.Raycast(touchPosition, hits, TrackableType.PlaneWithinPolygon)) {
+            var hitPose = hits[0].pose;
+            
+            if (spawnedObject == null) {
+                spawnedObject = Instantiate(prefabObject, hitPose.position, hitPose.rotation);
+            } else {
+                spawnedObject.transform.position = hitPose.position;
+            }
+
+            placementUpdate.Invoke();
+        }
+    }
+
+
+
+    bool TryGetTouchPosition(out Vector2 touchPosition) {
+        if (Input.touchCount > 0) {
+            touchPosition = Input.GetTouch(0).position;
+            return true;
+        }
+
+        touchPosition = default;
+        return false;
+    }
+
+    public void DisableVisual() {
+        visualObject.SetActive(false);
+    }
+}
